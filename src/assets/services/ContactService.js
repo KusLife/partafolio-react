68b9dd18f2b1455ed9cc6a78341acec0ce69573b
Kus <larisovkirill@gmail.com>
2024-12-16 21:21:17 +0100

    added all changes and ready to deploy

--- src/assets/services/ContactService.js
+++ src/assets/services/ContactService.js
@@ -1,10 +1,13 @@
 import emailjs from 'emailjs-com';
 
 export async function sendEmail(formData) {
+const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
   return await emailjs.send(
-    'service_j5ft7l8',
-    'template_wbo982c',
+    serviceId,
+    templateId,
     formData,
-    'wegXSo1hPZMcbaPfj'
+    userId
   );
 }
